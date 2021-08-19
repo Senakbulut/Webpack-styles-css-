2a7620e0554e2b5d631993371cabafe1c3b3db85
Sena Akbulut <Senakbulut@users.noreply.github.com>
2021-08-19 20:07:14 +0300

    webpack style config

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './style.scss'
+
+const headline = "Welcome to the webpage"
+document.querySelector('h1').innerText = headline
+// npm i -D css-loader style-loader    css build ve stiller için
